a5542854004f5f54c22055c531d0bc41d3ba25fb
VeltmanAlexey <104280303+VeltmanAlexey@users.noreply.github.com>
2022-09-05 17:21:13 +0300

    3.1.4

---
+++ Spring-Boot-Security-FetchAPI-master/src/main/java/ru/itsinfo/fetchapi/config/ApplicationSecurityConfig.java
@@ -0,0 +1,60 @@
+package ru.itsinfo.fetchapi.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.itsinfo.fetchapi.service.AppService;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // сервис, с помощью которого тащим пользователя
+    private final AppService appService;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final SuccessUserHandler successUserHandler;
+
+
+    @Autowired
+    public ApplicationSecurityConfig(AppService appService,
+                                     PasswordEncoder passwordEncoder,
+                                     SuccessUserHandler successUserHandler) {
+        this.appService = appService;
+        this.passwordEncoder = passwordEncoder;
+        this.successUserHandler = successUserHandler;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(appService).passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                // Декларирует, что все запросы к любой конечной точке должны быть авторизованы, иначе они должны быть отклонены
+                .authorizeRequests()
+                .antMatchers("/", "/img/**", "/css/**", "/js/**", "/webjars/**").permitAll()
+//                .antMatchers(HttpMethod.GET, "/api/users/*").hasRole("USER")
+                .antMatchers("/api/users/*", "/api/roles").hasRole("ADMIN")
+                .anyRequest().authenticated();
+//                .and().sessionManagement().disable(); // сообщает Spring, что не следует хранить информацию о сеансе для пользователей, поскольку это не нужно для API
+//                .and().httpBasic(); // сообщает Spring, чтобы он ожидал базовую HTTP аутентификацию
+
+        http.formLogin()
+                .successHandler(successUserHandler)
+                .loginPage("/") // указываем страницу с формой логина
+                .permitAll() // даем доступ к форме логина всем
+                .loginProcessingUrl("/login");
+        http.logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .permitAll();
+    }
+}
